ec243645eab7eb2c15c3782ca6869b9d3049f545
checha9i <checha.db63@gmail.com>
2018-08-17 11:30:58 -0600

    Respaldo por si la cago :V

--- src/proyecto1/XLSaXForm/LecturaExcel.java
+++ src/proyecto1/XLSaXForm/LecturaExcel.java
@@ -24,60 +24,243 @@ import org.apache.poi.ss.usermodel.Row;
 import proyecto1.XLSaXForm.Nodo;
 import proyecto1.XLSaXForm.Analizador.Analyzer;
 import proyecto1.utils.CrearArchivo;
+
 /**
  *
  * @author Javier Solares
  */
 public class LecturaExcel {
-CrearArchivo archivo=new CrearArchivo();
+
+    CrearArchivo archivo = new CrearArchivo();
 
     ArrayList<String> Encabezado = new ArrayList<String>();
     ArrayList<String> aux_list = new ArrayList<String>();
     StringBuffer buffer = new StringBuffer();
 
-    
-    public void Configuraciones(FileInputStream file) throws IOException {
-    
+    public void Configuraciones(FileInputStream file, int i) throws IOException {
+
     }
-    public void Opciones(FileInputStream file) throws IOException {
-    
+
+    public void Opciones(FileInputStream file, int i) throws IOException {
+        HSSFWorkbook workbook = new HSSFWorkbook(file);
+        /*
+	
+	 * Obtenemos la primera pestaña a la que se quiera procesar indicando el indice.
+	
+	 * Una vez obtenida la hoja excel con las filas que se quieren leer obtenemos el iterator
+	
+	 * que nos permite recorrer cada una de las filas que contiene.
+	
+         */
+        HSSFSheet sheet = workbook.getSheetAt(i);
+        Iterator<Row> rowIterator = sheet.iterator();
+        int contadorFila = 0;
+        int contadorCol = 0;
+        Row row;
+        boolean error = false;
+        int conterror = 0;
+
+        // Recorremos todas las filas para mostrar el contenido de cada celda
+        while (rowIterator.hasNext()) {
+            row = rowIterator.next();
+
+            // Obtenemos el iterator que permite recorres todas las celdas de una fila
+            Iterator<Cell> cellIterator = row.cellIterator();
+            Cell celda;
+            contadorCol = 0;
+            while (cellIterator.hasNext()) {
+                celda = cellIterator.next();
+                if (contadorFila == 0) {
+                    Encabezado.add(celda.getStringCellValue());
+                } else {
+                    // Dependiendo del formato de la celda el valor se debe mostrar como String, Fecha, boolean, entero...
+                    switch (celda.getCellType()) {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.println(celda.getStringCellValue());
+                            contadorCol++;
+                            break;
+
+                        case Cell.CELL_TYPE_STRING:
+                            aux_list.add(celda.getStringCellValue());
+                            contadorCol++;
+                            break;
+                        case Cell.CELL_TYPE_BOOLEAN:
+                            if (celda.getBooleanCellValue() == true) {
+                                aux_list.add("true");
+                            } else {
+                                aux_list.add("false");
+                            }
+                            contadorCol++;
+                            break;
+                        default:
+                            // System.out.println("false");
+                            aux_list.add("Viene-Vacio");
+                            contadorCol++;
+
+                    }
+
+                }
+
+            }
+            if (contadorFila > 0) {
+                error = ordenar_opciones(Encabezado, aux_list);
+                if (error == true) {
+                    conterror++;
+                }
+                aux_list.clear();
+            }
+            contadorFila++;
+        }
+
+        // cerramos el libro excel	
+        workbook.close();
+
+        if (conterror == 0) {
+            archivo.CrearFile(buffer.toString());
+        } else {
+            conterror = 0;
+            error = false;
+            System.out.println("ERROR");
+        }
     }
-    
+
     public boolean Verificaciones(FileInputStream file) throws IOException {
-        boolean validacion=false;
-    HSSFWorkbook workbook = new HSSFWorkbook(file);
-    System.out.println(workbook.getNumberOfSheets());
+        boolean validacion = false;
+        HSSFWorkbook workbook = new HSSFWorkbook(file);
+        System.out.println(workbook.getNumberOfSheets());
         HSSFSheet sheet = workbook.getSheetAt(0);
-    HSSFSheet sheet1 = workbook.getSheetAt(1);
-        switch(workbook.getNumberOfSheets()){
-        case 2:
-            
-            break;
-        case 3:
-    
-    HSSFSheet sheet2 = workbook.getSheetAt(2);
-            
-            break;
-        default:
-            System.out.println("Error");
-            return false;
-    
+        HSSFSheet sheet1 = workbook.getSheetAt(1);
+        HSSFSheet sheet2 = workbook.getSheetAt(2);
+        boolean hojaencuesta = false, hojaopciones = false, hojaconfig = false;
+        int numencuesta = 0, numopciones = 0, numconfig = 0;
+        switch (workbook.getNumberOfSheets()) {
+            case 2:
+
+                //empezamos verificando si existe la hoja encuesta
+                if (sheet.getSheetName().equalsIgnoreCase("encuesta") && hojaencuesta == false) {
+                    hojaencuesta = true;
+                    numencuesta = 0;
+                    validacion = true;
+                } else if (sheet1.getSheetName().equalsIgnoreCase("encuesta") && hojaencuesta == false) {
+                    hojaencuesta = true;
+                    numencuesta = 1;
+                    validacion = true;
+                } else {//si ninguna de las 2 hojas es encuesta mostramos error
+                    System.out.println("Error, No existe la hoja encuesta");
+                }
+
+                //verificamos ahora si la hoja opciones existe
+                if (sheet.getSheetName().equalsIgnoreCase("opciones") && hojaencuesta == false) {
+                    hojaencuesta = true;
+                    numopciones = 0;
+                    validacion = true;
+                } else if (sheet1.getSheetName().equalsIgnoreCase("opciones") && hojaencuesta == false) {
+                    hojaencuesta = true;
+                    validacion = true;
+                    numopciones = 1;
+                } else {
+                    System.out.println("Error, No existe la hoja opciones");
+                }
+                //si las dos son true el archivo es correcto y procedemos a realizar el parseo
+                if (hojaencuesta && hojaopciones) {
+                    encuesta(file, numencuesta);
+                    Opciones(file, numopciones);
+                } else {
+                    System.out.println("Algo esta mal.");
+                }
+
+                break;
+            case 3:
+                //empezamos verificando si existe la hoja encuesta
+                if (sheet.getSheetName().equalsIgnoreCase("encuesta") && hojaencuesta == false) {
+                    hojaencuesta = true;
+                    numencuesta = 0;
+                    validacion = true;
+                } else if (sheet1.getSheetName().equalsIgnoreCase("encuesta") && hojaencuesta == false) {
+                    hojaencuesta = true;
+                    numencuesta = 1;
+                    validacion = true;
+                } else if (sheet2.getSheetName().equalsIgnoreCase("encuesta") && hojaencuesta == false) {
+                    hojaencuesta = true;
+                    numencuesta = 2;
+                    validacion = true;
+                } else {//si ninguna de las 3 hojas es encuesta mostramos error
+                    System.out.println("Error, No existe la hoja encuesta");
+                }
+
+                //verificamos ahora si la hoja opciones existe
+                if (sheet.getSheetName().equalsIgnoreCase("opciones") && hojaencuesta == false) {
+                    hojaopciones = true;
+                    numopciones = 0;
+
+                } else if (sheet1.getSheetName().equalsIgnoreCase("opciones") && hojaencuesta == false) {
+                    hojaopciones = true;
+
+                    numopciones = 1;
+                } else if (sheet2.getSheetName().equalsIgnoreCase("opciones") && hojaencuesta == false) {
+                    hojaopciones = true;
+
+                    numopciones = 2;
+                } else {
+                    System.out.println("Error, No existe la hoja opciones");
+                }
+
+                //verificamos ahora si la hoja opciones existe
+                if (sheet.getSheetName().equalsIgnoreCase("configuracion") && hojaencuesta == false) {
+                    hojaconfig = true;
+                    numopciones = 0;
+
+                } else if (sheet1.getSheetName().equalsIgnoreCase("configuracion") && hojaencuesta == false) {
+                    hojaconfig = true;
+
+                    numopciones = 1;
+                } else if (sheet2.getSheetName().equalsIgnoreCase("configuracion") && hojaencuesta == false) {
+                    hojaconfig = true;
+
+                    numopciones = 2;
+                } else {
+                    System.out.println("Error, No existe la hoja Configuracion");
+                }
+                //si las dos son true el archivo es correcto y procedemos a realizar el parseo
+                if (hojaencuesta && hojaopciones && hojaconfig) {
+                    encuesta(file, numencuesta);
+                    Opciones(file, numopciones);
+                    Configuraciones(file, numconfig);
+                } else {
+                    System.out.println("Algo esta mal.");
+                }
+
+                break;
+            default:
+                System.out.println("Error numero invalido de hojas");
+                return false;
+
+        }
+
+        return validacion;
     }
-    
+
+      boolean ordenar_opciones(ArrayList<String> Encabezado, ArrayList<String> aux_List) {
+        ArrayList<String> auxEncabezado = new ArrayList<String>();
+        ArrayList<String> auxOrdenado = new ArrayList<String>();
+        for (int i = 0; i < Encabezado.size(); i++) {
+            if (Encabezado.get(i).equalsIgnoreCase("Tipo")) {
+                auxEncabezado.add(Encabezado.get(i));
+                auxOrdenado.add(aux_List.get(i));
+            }
+        }
+        for (int i = 0; i < Encabezado.size(); i++) {
+            if (!Encabezado.get(i).equalsIgnoreCase("Tipo")) {
+                auxEncabezado.add(Encabezado.get(i));
+                auxOrdenado.add(aux_List.get(i));
+            }
+        }
+        return añadir_encuesta(auxEncabezado, auxOrdenado);
+    }
 
     
-    if(sheet.getSheetName().equalsIgnoreCase("encuesta")){
-        validacion=true;
     
-    }
-        
-        
-    
-    
-    return validacion;
-    }
-    
-    public void Ordenar(FileInputStream file) throws IOException {
+    public void encuesta(FileInputStream file, int hoja) throws IOException {
         //Nodo padre, hijo;
         // Crear el objeto que tendra el libro de Excel
         HSSFWorkbook workbook = new HSSFWorkbook(file);
@@ -94,20 +277,19 @@ CrearArchivo archivo=new CrearArchivo();
 	 * que nos permite recorrer cada una de las filas que contiene.
 	
          */
-        HSSFSheet sheet = workbook.getSheetAt(0);
+        HSSFSheet sheet = workbook.getSheetAt(hoja);
 
         Iterator<Row> rowIterator = sheet.iterator();
 
         /**
          *
          */
-        int contadorCol = 0;
         int contadorFila = 0;
-        int contadorAux = 0;
+        int contadorCol = 0;
         Row row;
         boolean error = false;
         int conterror = 0;
-        int selecciona1=0;
+
         // Recorremos todas las filas para mostrar el contenido de cada celda
         while (rowIterator.hasNext()) {
             //Nodo hijo=new Nodo("Linea",0,0,"Linea");
@@ -165,7 +347,7 @@ CrearArchivo archivo=new CrearArchivo();
 
             }
             if (contadorFila > 0) {
-                error = ordenar_Ingresar(Encabezado, aux_list);
+                error = ordenar_encuesta(Encabezado, aux_list);
                 if (error == true) {
                     conterror++;
                 }
@@ -187,13 +369,13 @@ CrearArchivo archivo=new CrearArchivo();
     }
     public int estado = 0, estadociclo = 0;
 
-    boolean ordenar_Ingresar(ArrayList<String> Encabezado, ArrayList<String> aux_List) {
+    boolean ordenar_encuesta(ArrayList<String> Encabezado, ArrayList<String> aux_List) {
         ArrayList<String> auxEncabezado = new ArrayList<String>();
         ArrayList<String> auxOrdenado = new ArrayList<String>();
         for (int i = 0; i < Encabezado.size(); i++) {
             if (Encabezado.get(i).equalsIgnoreCase("Tipo")) {
                 auxEncabezado.add(Encabezado.get(i));
-             auxOrdenado.add(aux_List.get(i));
+                auxOrdenado.add(aux_List.get(i));
             }
         }
         for (int i = 0; i < Encabezado.size(); i++) {
@@ -202,10 +384,10 @@ CrearArchivo archivo=new CrearArchivo();
                 auxOrdenado.add(aux_List.get(i));
             }
         }
-        return añadir(auxEncabezado, auxOrdenado);
+        return añadir_encuesta(auxEncabezado, auxOrdenado);
     }
 
-    boolean añadir(ArrayList<String> Encabezado, ArrayList<String> aux_List) {
+    boolean añadir_encuesta(ArrayList<String> Encabezado, ArrayList<String> aux_List) {
 
         for (int i = 0; i < Encabezado.size(); i++) {
             if (Encabezado.get(i).equalsIgnoreCase("Tipo")) {
@@ -225,7 +407,7 @@ CrearArchivo archivo=new CrearArchivo();
 
                     estado++;
                     buffer.append("Finalizar Agrupacion{\n");
-               
+
                 } else if (aux_List.get(i).equalsIgnoreCase("Iniciar ciclo")) {
                     String cad = aux_List.get(i).toString();
 
@@ -239,26 +421,24 @@ CrearArchivo archivo=new CrearArchivo();
 
                 } else {
                     String cad = aux_List.get(i).toString();
-                    String aux="";
-                    boolean primertoken=false;
-                    for(int n=0; n <cad.length(); n++) {
-                     aux+=cad.charAt(n);
-                    if(aux.equals("selecciona_uno")){
-                    buffer.append(aux);
-                    aux="";
-                    primertoken=true;
-                    }
-                    
-                     }
-                    if(primertoken){
-                        aux=aux.replace(" ", "/");
-                    buffer.append(aux+"{\n");
-                    }else{
-                    buffer.append(cad+"{\n");
-                    }
-                    
+                    String aux = "";
+                    boolean primertoken = false;
+                    for (int n = 0; n < cad.length(); n++) {
+                        aux += cad.charAt(n);
+                        if (aux.equals("selecciona_uno")) {
+                            buffer.append(aux);
+                            aux = "";
+                            primertoken = true;
+                        }
 
-                    
+                    }
+                    if (primertoken) {
+                        aux = aux.replace(" ", "/");
+                        buffer.append(aux + "{\n");
+                    } else {
+                        buffer.append(cad + "{\n");
+                    }
+
                 }
 
             } else if (Encabezado.get(i).equalsIgnoreCase("idpregunta")) {
@@ -322,21 +502,19 @@ CrearArchivo archivo=new CrearArchivo();
                 if (!aux_List.get(i).equals("Viene-Vacio")) {
                     buffer.append("codigo_post~#~" + aux_List.get(i) + "~#~\n");
                 }
-            }  else if (Encabezado.get(i).equalsIgnoreCase("multimedia")) {
+            } else if (Encabezado.get(i).equalsIgnoreCase("multimedia")) {
                 if (!aux_List.get(i).equals("Viene-Vacio")) {
                     buffer.append("multimedia~#~" + aux_List.get(i) + "~#~\n");
                 }
-            }
-             else if (Encabezado.get(i).equalsIgnoreCase("decimal")) {
+            } else if (Encabezado.get(i).equalsIgnoreCase("decimal")) {
                 if (!aux_List.get(i).equals("Viene-Vacio")) {
                     buffer.append("decimal~#~" + aux_List.get(i) + "~#~\n");
                 }
-            }
-               else if (Encabezado.get(i).equalsIgnoreCase("hora")) {
+            } else if (Encabezado.get(i).equalsIgnoreCase("hora")) {
                 if (!aux_List.get(i).equals("Viene-Vacio")) {
                     buffer.append("hora~#~" + aux_List.get(i) + "~#~\n");
                 }
-            }   else if (Encabezado.get(i).equalsIgnoreCase("fechahora")) {
+            } else if (Encabezado.get(i).equalsIgnoreCase("fechahora")) {
                 if (!aux_List.get(i).equals("Viene-Vacio")) {
                     buffer.append("fechahora~#~" + aux_List.get(i) + "~#~\n");
                 }
@@ -344,12 +522,11 @@ CrearArchivo archivo=new CrearArchivo();
 
         }
 
-   
-              buffer.append("} \n");
-            estado = 0;
-            estadociclo--;
-            estadociclo--;
-        
+        buffer.append("} \n");
+        estado = 0;
+        estadociclo--;
+        estadociclo--;
+
         return false;
     }
 
